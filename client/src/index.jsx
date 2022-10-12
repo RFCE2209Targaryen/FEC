98389c7c63551f1cb0d6e05c1544403f601e6bba
Nicholas Amenda <Nickamenda@gmail.com>
2022-10-12 14:29:54 -0500

    create and connect css sheet and create Star Review component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,6 @@
+
 import React from 'react';
+import axios from "axios";
 import ReactDOM from 'react-dom/client';
 import Product from './components/productComponent/Product.jsx'
 import QandA from './components/questionsComponent/QandA.jsx'
@@ -18,6 +20,9 @@ const App = () => {
     </>
   )
 }
-
+axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/products?page=1&count=5`, {headers: {'Authorization': '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'}})
+.then((res) => {
+  console.log(res)
+})
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
